16748d6834d7e5712b60f6f406c2f7822501719b
Haider <headeralishah@gmail.com>
2017-01-29 14:20:20 +0500

    bid placement done.

--- src/app/bidding/bidding.component.ts
+++ src/app/bidding/bidding.component.ts
@@ -16,10 +16,35 @@ declare var firebase: any;
 export class BiddingComponent implements OnInit {
 
   biddingData: any;
+  bids: any = [];
   constructor(private route: ActivatedRoute, public dataService: DataService, private router: Router) {
     this.biddingData = localStorage.getItem("auctionDetails");
     this.biddingData = JSON.parse(this.biddingData);
     console.log(this.biddingData);
+    // let email = firebase.auth().currentUser;
+    // console.log(email, 'email');
+
+    firebase.database().ref('/bids').on('child_added', (data) => {
+      // this.allAuctioneeries = [];
+      let obj: any = data.val();
+      obj.id = data.key;
+      // obj.isoStartTime = new Date(obj.startTimeInMilliSeconds);
+      // obj.isoEndTime = new Date(obj.endTimeInMilliSeconds);
+      // let rightNow = new Date().getTime();
+
+      // this.bids.push(obj)
+      // console.log(this.bids, 'this.bids');
+
+      if (obj.productId == this.biddingData.id) {
+
+        this.bids.push(obj)
+        console.log(this.bids, 'this.bids');
+
+      }
+    })
+
+
+
   }
 
   ngOnInit() {
@@ -47,22 +72,48 @@ export class BiddingComponent implements OnInit {
         this.warning.message = ''
       }, 5000);
     }
+
+
     else {
+      let rightNow = new Date().getTime();
 
-      this.bid.creatorId = biddingData.creatorId;
-      this.bid.productId = biddingData.id;
-      this.bid.placerId = firebase.auth().currentUser.uid;
-      this.bid.amount = this.bidamount;
+      console.log(rightNow, 'rightNow');
+      console.log(biddingData.endTimeInMilliSeconds, 'biddingData.endTimeInMilliSeconds')
+      if (rightNow > biddingData.endTimeInMilliSeconds) {
+        this.warning.status = true;
+        this.warning.msg = 'Sorry the bidding time has ended and product awarded.'
+        setTimeout(() => {
+          this.warning.status = false;
+          this.warning.message = ''
+        }, 5000);
+      } else {
+        this.bid.creatorId = biddingData.creatorId;
+        this.bid.productId = biddingData.id;
+        this.bid.placerId = firebase.auth().currentUser.uid;
+        this.bid.placerEmail = firebase.auth().currentUser.email;
+        this.bid.amount = this.bidamount;
 
-      console.log(this.bid, 'this.bid ***********************');
 
-      console.log('amount bidded');
-      firebase.database().ref('bids/').push(this.bid)
-        .then((v) => {
+        console.log(this.bid, 'this.bid ***********************');
 
-          this.bid = {};
-          console.log(v, '**************');
-        })
+        console.log('amount bidded');
+
+
+        biddingData.firstbiddingamount = this.bidamount;
+
+        firebase.database().ref('auctioneeri/' + this.bid.productId).set(biddingData);
+
+
+
+        firebase.database().ref('bids/').push(this.bid)
+          .then((v) => {
+
+            this.bid = {};
+            console.log(v, '**************');
+          })
+
+      }
+
 
 
 
